e59fdb3f9af8d6b30032f7dc19ae484465dd8801
IT 김대윤 <dykim10@eyes.co.kr>
2024-01-29 20:27:17 +0900

    유효성검사1

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -3,12 +3,13 @@
         TEST :: header
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="container p-5">
-        <div>
-            <h1>글쓰기</h1>
-            <form action="/articles" method="POST">
-                <input type="text" name="contents">
-                <input type="button" value="저장하기">
+    <body>
+        <div class="container p-5">
+            <h1 class="text-2xl">글쓰기</h1>
+            <form action="/articles" method="POST" class="mt-3">
+                @csrf
+                <input type="text" name="contents" class="block w-auto mb-2 rounded">
+                <button class="py-1 px-3 w-full text-xs rounded-full">저장하기</button>
             </form>
         </div>
     </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,4 +33,16 @@ require __DIR__.'/auth.php';
 
 route::get('/articles/create', function(){
     return view('articles/create');
+});
+
+route::post('/articles', function(Request $request){
+    //비어있지않고, 문자열이고, 255자를 넘으면 안된다.
+    $request->validate([
+        'contents' => [
+            'required',
+            'string',
+            'max:255'
+        ]
+    ]);
+    return 'hello';
 });
